c422b514173aec09fb3b00c267e4fae045ec73db
alphanumeric-username <geometrianaoeuclidiana@gmail.com>
2025-07-14 09:50:22 -0300

    add: function declarations for vector reflectivity vp and rho gradient operators

--- examples/seismic/vector_reflectivity/operators.py
+++ examples/seismic/vector_reflectivity/operators.py
@@ -93,90 +93,67 @@ def AdjointOperator(model, geometry, space_order=4,
                     name='Adjoint', **kwargs)
 
 
-# def GradientOperator(model, geometry, space_order=4, save=True,
-#                      kernel='OT2', **kwargs):
-#     """
-#     Construct a gradient operator in an acoustic media.
+def GradientOperator_Vp(model, geometry, space_order=4, save=True,
+                     kernel='OT2', **kwargs):
+    """
+    Construct a gradient operator in an acoustic media.
 
-#     Parameters
-#     ----------
-#     model : Model
-#         Object containing the physical parameters.
-#     geometry : AcquisitionGeometry
-#         Geometry object that contains the source (SparseTimeFunction) and
-#         receivers (SparseTimeFunction) and their position.
-#     space_order : int, optional
-#         Space discretization order.
-#     save : int or Buffer, optional
-#         Option to store the entire (unrolled) wavefield.
-#     kernel : str, optional
-#         Type of discretization, centered or shifted.
-#     """
-#     m = model.m
+    Parameters
+    ----------
+    model : Model
+        Object containing the physical parameters.
+    geometry : AcquisitionGeometry
+        Geometry object that contains the source (SparseTimeFunction) and
+        receivers (SparseTimeFunction) and their position.
+    space_order : int, optional
+        Space discretization order.
+    save : int or Buffer, optional
+        Option to store the entire (unrolled) wavefield.
+    kernel : str, optional
+        Type of discretization, centered or shifted.
+    """
+    m = model.m
 
-#     # Gradient symbol and wavefield symbols
-#     grad = Function(name='grad', grid=model.grid)
-#     u = TimeFunction(name='u', grid=model.grid, save=geometry.nt if save
-#                      else None, time_order=2, space_order=space_order)
-#     v = TimeFunction(name='v', grid=model.grid, save=None,
-#                      time_order=2, space_order=space_order)
-#     rec = geometry.rec
+    # Gradient symbol and wavefield symbols
+    grad = Function(name='grad', grid=model.grid)
+    u = TimeFunction(name='u', grid=model.grid, save=geometry.nt if save
+                     else None, time_order=2, space_order=space_order)
+    v = TimeFunction(name='v', grid=model.grid, save=None,
+                     time_order=2, space_order=space_order)
+    rec = geometry.rec
 
-#     s = model.grid.stepping_dim.spacing
-#     eqn = iso_stencil(v, model, kernel, forward=False)
+    s = model.grid.stepping_dim.spacing
+    eqn = iso_stencil(v, model, kernel, forward=False)
 
-#     if kernel == 'OT2':
-#         gradient_update = Inc(grad, - u * v.dt2)
-#     elif kernel == 'OT4':
-#         gradient_update = Inc(grad, - u * v.dt2 - s**2 / 12.0 * u.biharmonic(m**(-2)) * v)
-#     # Add expression for receiver injection
-#     receivers = rec.inject(field=v.backward, expr=rec * s**2 / m)
+    if kernel == 'OT2':
+        gradient_update = Inc(grad, - u * v.dt2)
+    elif kernel == 'OT4':
+        gradient_update = Inc(grad, - u * v.dt2 - s**2 / 12.0 * u.biharmonic(m**(-2)) * v)
+    # Add expression for receiver injection
+    receivers = rec.inject(field=v.backward, expr=rec * s**2 / m)
 
-#     # Substitute spacing terms to reduce flops
-#     return Operator(eqn + receivers + [gradient_update], subs=model.spacing_map,
-#                     name='Gradient', **kwargs)
+    # Substitute spacing terms to reduce flops
+    return Operator(eqn + receivers + [gradient_update], subs=model.spacing_map,
+                    name='Gradient', **kwargs)
 
 
-# def BornOperator(model, geometry, space_order=4,
-#                  kernel='OT2', **kwargs):
-#     """
-#     Construct an Linearized Born operator in an acoustic media.
+def GradientOperator_R(model, geometry, space_order=4, save=True,
+                     kernel='OT2', **kwargs):
+    """
+    Construct a gradient operator in an acoustic media.
 
-#     Parameters
-#     ----------
-#     model : Model
-#         Object containing the physical parameters.
-#     geometry : AcquisitionGeometry
-#         Geometry object that contains the source (SparseTimeFunction) and
-#         receivers (SparseTimeFunction) and their position.
-#     space_order : int, optional
-#         Space discretization order.
-#     kernel : str, optional
-#         Type of discretization, centered or shifted.
-#     """
-#     m = model.m
-
-#     # Create source and receiver symbols
-#     src = geometry.src
-#     rec = geometry.rec
-
-#     # Create wavefields and a dm field
-#     u = TimeFunction(name="u", grid=model.grid, save=None,
-#                      time_order=2, space_order=space_order)
-#     U = TimeFunction(name="U", grid=model.grid, save=None,
-#                      time_order=2, space_order=space_order)
-#     dm = Function(name="dm", grid=model.grid, space_order=0)
-
-#     s = model.grid.stepping_dim.spacing
-#     eqn1 = iso_stencil(u, model, kernel)
-#     eqn2 = iso_stencil(U, model, kernel, q=-dm*u.dt2)
-
-#     # Add source term expression for u
-#     source = src.inject(field=u.forward, expr=src * s**2 / m)
-
-#     # Create receiver interpolation expression from U
-#     receivers = rec.interpolate(expr=U)
-
-#     # Substitute spacing terms to reduce flops
-#     return Operator(eqn1 + source + eqn2 + receivers, subs=model.spacing_map,
-#                     name='Born', **kwargs)
+    Parameters
+    ----------
+    model : Model
+        Object containing the physical parameters.
+    geometry : AcquisitionGeometry
+        Geometry object that contains the source (SparseTimeFunction) and
+        receivers (SparseTimeFunction) and their position.
+    space_order : int, optional
+        Space discretization order.
+    save : int or Buffer, optional
+        Option to store the entire (unrolled) wavefield.
+    kernel : str, optional
+        Type of discretization, centered or shifted.
+    """
+    pass
